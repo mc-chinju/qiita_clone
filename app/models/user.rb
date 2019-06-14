262a6178d5a2eb77c6f507cf9386cb61825bfbaf
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2019-06-14 18:31:11 +0900

    fix for "undefined method 'devise'"
    ref: https://github.com/lynndylanhurley/devise_token_auth/issues/1276

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,7 @@
 #
 
 class User < ApplicationRecord
+  extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
