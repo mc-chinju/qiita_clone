a781f3335ccb944a6de8589310069879fde0258a
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2019-06-25 23:22:28 +0900

    Turn off CSRF
    トークン認証ではCSRFを使わないため設定をOFF

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,4 +1,3 @@
 class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
-  # MEMO: HTTP Client 経由で操作する場合にコメントインする
-  # protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session
 end
