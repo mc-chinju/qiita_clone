4ba3d3b2bc4835ca2b4c0b15256986fc0ca3be02
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2019-06-26 14:10:52 +0900

    fix articles api
    記事一覧のAPIではbodyを返さないように修正
    更新順に並び替えてデータを返すように修正

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe "Articles", type: :request do
   describe "GET /articles" do
     subject { get(api_v1_articles_path) }
 
-    before { create_list(:article, 3) }
+    let!(:article1) { create(:article, updated_at: 1.days.ago) }
+    let!(:article2) { create(:article, updated_at: 2.days.ago) }
+    let!(:article3) { create(:article) }
 
-    it "記事の一覧が取得できる" do
+    it "記事の一覧が取得できる(更新順)" do
       subject
 
       res = JSON.parse(response.body)
@@ -14,7 +16,8 @@ RSpec.describe "Articles", type: :request do
       aggregate_failures do
         expect(response).to have_http_status(:ok)
         expect(res.length).to eq 3
-        expect(res[0].keys).to eq ["id", "title", "body", "user"]
+        expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
+        expect(res[0].keys).to eq ["id", "title", "user"]
         expect(res[0]["user"].keys).to eq ["id", "name", "email"]
       end
     end
