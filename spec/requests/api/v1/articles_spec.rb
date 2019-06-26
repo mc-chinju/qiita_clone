57948a01831da41388f18099e8e56ef734ed6209
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2019-06-26 17:34:26 +0900

    Add ariticles show api

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -40,6 +40,7 @@ RSpec.describe "Articles", type: :request do
           expect(res["id"]).to eq article.id
           expect(res["title"]).to eq article.title
           expect(res["body"]).to eq article.body
+          expect(res["updated_at"]).to be_present
           expect(res["user"]["id"]).to eq article.user.id
           expect(res["user"].keys).to eq ["id", "name", "email"]
         end
