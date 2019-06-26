9ccdeb56b5f766d439d65a45e5f96a24b9e9cd89
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2019-06-26 15:32:26 +0900

    add 'updated_at' to articles api response

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe "Articles", type: :request do
         expect(response).to have_http_status(:ok)
         expect(res.length).to eq 3
         expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
-        expect(res[0].keys).to eq ["id", "title", "user"]
+        expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
         expect(res[0]["user"].keys).to eq ["id", "name", "email"]
       end
     end
